304cb03b4affddd493aef0853c1b70f87a7e307f
jrummuka <jrummuka@student.hive.fi>
2024-10-07 18:59:30 +0300

    all routes redir

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,6 +33,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-venues", myVenuesRoutes);
 
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
+
 app.listen(7001, () => {
     console.log("Server is running on port 7001");
 });
